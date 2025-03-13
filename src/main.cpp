0269019a7ef0c5c7afe5324e73027d4fc39d80c1
aaronl647 <aaron.l@live.ca>
2025-03-13 15:43:46 -0400

    added test motor movement

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,21 @@
 #define IN3 21
 #define IN4 22
 
-void setup() {
-    Serial.begin(115200);  // Start serial communication
+Stepper stepper(STEPS_PER_REV, IN1, IN3, IN2, IN4);
+
+void setup()
+{
+  stepper.setSpeed(10); //Set RPM
+  Serial.begin(115200); // Start serial communication
+  Serial.println("Stepper Motor Test"); //Print initial message
 }
 
 void loop() {
-    Serial.println("Hello, World!");  // Print message
+    Serial.println("Moving forward"); // Print message
+    stepper.step(STEPS_PER_REV); // Move 1 full revolution forward
     delay(1000);  // Wait 1 second
+
+    Serial.println("Moving backwards");
+    stepper.step(-STEPS_PER_REV); // Move 1 full revolution backwards
+    delay(1000);
 }

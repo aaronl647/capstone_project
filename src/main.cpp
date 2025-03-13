f5674adda2e54e6956c7cec0afc69e27825cdbf9
aaronl647 <aaron.l@live.ca>
2025-03-13 15:15:19 -0400

    added stepper driver libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <Stepper.h>
+
+//Define motor steps per revolution
+#define STEPS_PER_REV 2048 //specific to 28BYJ-48 motor
+
+#define IN1 18
+#define IN2 19
+#define IN3 21
+#define IN4 22
 
 void setup() {
     Serial.begin(115200);  // Start serial communication

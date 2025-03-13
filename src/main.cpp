461b8c0518b77f3ce412c92229d070152c23d68c
aaronl647 <aaron.l@live.ca>
2025-03-13 14:58:24 -0400

    hello world

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);  // Start serial communication
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    Serial.println("Hello, World!");  // Print message
+    delay(1000);  // Wait 1 second
 }
